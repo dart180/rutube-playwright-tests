94d12eb617020bbce45173389b462d2cfaca1c3e
mir.la <mir.la@skbkontur.ru>
2025-04-08 01:59:05 +0500

    add mainpage fuxture

--- tests/specs/unauthorizaded/mainPage.spec.ts
+++ tests/specs/unauthorizaded/mainPage.spec.ts
@@ -1,21 +1,12 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
-test('Проверка доступности элементов хедера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaShapshot();
 });
-test('Проверка доступности элементов табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaShapshot();
 });
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaShapshot();
 });
